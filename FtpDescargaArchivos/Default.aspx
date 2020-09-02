1a137cb0e389cb080f2236e0cda6e78dcc39b934
Fernando Estrada <develofer.estrada@gmail.com>
2020-09-02 17:38:58 -0500

    Agregar archivos de proyecto.

---
+++ FtpDescargaArchivos/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FtpDescargaArchivos.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Ftp Descarga de Archivos</h1>
+            <br />
+            <input type="text" runat="server" name="archivo" id="archivo" placeholder="Nombre del Archivo" />
+            <br />
+            <asp:Button ID="btnDownload" runat="server" Text="Descargar Archivo" OnClick="btnDownload_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ FtpDescargaArchivos/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FtpDescargaArchivos
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnDownload_Click(object sender, EventArgs e)
+        {
+            string fileName = archivo.Value;
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://107.180.46.193/httpdocs/FTPFiles/" + fileName);
+
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+            request.Credentials = new NetworkCredential("usuario", "password");
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            using(MemoryStream stream=new MemoryStream())
+            {
+                response.GetResponseStream().CopyTo(stream);
+                Response.AddHeader("content-disposition", "attachment;filename=" + fileName);
+                Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                Response.BinaryWrite(stream.ToArray());
+                Response.End();
+            }
+
+        }
+    }
+}
